c468c71f0c4b9354c57180a6fd001cb25e24de5a
SagaTheGreat <ajay.patel305@gmail.com>
2018-08-26 01:41:20 +0530

    SigQueue demo
    NAME
           sigqueue - queue a signal and data to a process
    
    SYNOPSIS
           #include <signal.h>
    
           int sigqueue(pid_t pid, int sig, const union sigval value);
    
       Feature Test Macro Requirements for glibc (see feature_test_macros(7)):
    
           sigqueue(): _POSIX_C_SOURCE >= 199309L
    
    DESCRIPTION
           sigqueue()  sends  the  signal specified in sig to the process whose PID is given in pid.  The permissions required to send a signal
           are the same as for kill(2).  As with kill(2), the null signal (0) can be used to check if a process with a given PID exists.
    
           The value argument is used to specify an accompanying item of data (either an integer or a pointer value) to be sent with  the  sig‐
           nal, and has the following type:
    
               union sigval {
                   int   sival_int;
                   void *sival_ptr;
               };
    
           If  the receiving process has installed a handler for this signal using the SA_SIGINFO flag to sigaction(2), then it can obtain this
           data via the si_value field of the siginfo_t structure passed as the second argument to the handler.  Furthermore, the si_code field
           of that structure will be set to SI_QUEUE.

---
+++ sigQdemo.cpp
@@ -0,0 +1,78 @@
+/*
+
+Demo of sigqueue.
+
+SagaTheReallyGreat@SagaTheReallyGreat-Inspiron-5520:~/Practice/data_structure$ c++ -g sigQdemo.cpp -lpthread -o test
+SagaTheReallyGreat@SagaTheReallyGreat-Inspiron-5520:~/Practice/data_structure$ ./test
+Signal handled and waiting for the signal for child thread
+Signal sent successfully!!
+Signal No: 12 Data: SagaTheGreat
+SagaTheReallyGreat@SagaTheReallyGreat-Inspiron-5520:~/Practice/data_structure$ 
+
+*/
+
+#include<iostream>
+#include<signal.h>
+#include<cstdio>
+#include<unistd.h>
+#include<cstring>
+#include<cstdlib>
+#include <sys/types.h>
+#include<pthread.h>
+
+using namespace std;
+
+void handler(int sig, siginfo_t *info, void *arg)
+{
+	void *data = info->si_value.sival_ptr;
+	cout<<"Signal No: "<<sig<<" Data: "<<(char*)data<<endl;
+}
+
+void *myFun(void *var)
+{
+	union sigval value;
+	int pid = *((int *) var);
+
+	char *p = new char[20];
+	strcpy(p,"SagaTheGreat");
+	value.sival_ptr = (void*) p;
+	if(sigqueue(pid, SIGUSR2, value) == 0)
+	{
+		cout<<"Signal sent successfully!!"<<endl;
+	}
+	else
+	{
+		perror("SIGSENT-ERROR:");
+	}
+
+	sleep(1);
+	delete p;
+	p = NULL;
+
+	return NULL;
+}
+
+int main()
+{
+	pthread_t thread_id;
+	int pid;
+	struct sigaction action;
+
+	action.sa_flags = SA_SIGINFO;
+	action.sa_sigaction = &handler;
+
+	if (sigaction(SIGUSR2, &action, NULL) == -1)
+	{
+		perror("sigusr: sigaction");
+		return 0;
+	}
+	pid = getpid();
+
+	cout<<"Signal handled and waiting for the signal for child thread"<<endl;
+
+	pthread_create(&thread_id, NULL, myFun, &pid);
+
+	pthread_join(thread_id, NULL);
+
+	return 0;
+}
